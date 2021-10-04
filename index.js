190d58d9b646cda10f203a315a008f2fc4a93092
Oliveira <jonata.h.oliveira@accenture.com>
2021-10-04 16:28:08 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const { ApolloServer } = require("apollo-server");
+const { readFileSync } = require('fs')
+
+const oAuthApplicationAuthenticate = require("./responses/oAuthApplicationAuthenticate.json")
+const locations = require('./responses/locations.json')
+const transactionExternalPaymentStart = require('./responses/startTransaction.json')
+const transactionExternalPaymentCancel = require('./responses/cancelTransaction.json')
+const transactionExternalPaymentFinalize = require('./responses/finalizeTransaction.json')
+const transactions = require('./responses/statusTransaction.json')
+
+
+const typeDefs = readFileSync('./typeDefs/typeDefs.graphql').toString('utf-8');
+
+const resolvers = {
+  Query: {
+    oAuthApplicationAuthenticate: () => oAuthApplicationAuthenticate.data.oAuthApplicationAuthenticate,
+    locations: () => locations.data.locations,
+    transactionExternalPaymentStart: () => transactionExternalPaymentStart.data.transactionExternalPaymentStart,
+    transactionExternalPaymentCancel: () => transactionExternalPaymentCancel.data.transactionExternalPaymentCancel,
+    transactionExternalPaymentFinalize: () => transactionExternalPaymentFinalize.data.transactionExternalPaymentFinalize,
+    transactions: () => transactions.data.transactions
+  }
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  mocks: true,
+  mockEntireSchema: false
+});
+
+server.listen().then(({ url }) => console.log(`Server running on port ${url}`));
